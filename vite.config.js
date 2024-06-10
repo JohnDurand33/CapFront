fb512ac100514b7b09295d59efbd8f5be530fce1
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-06-10 12:11:49 -0600

    hosting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-export default {
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
     build: {
-        outDir: "dist",
+        chunkSizeWarningLimit: 1000,
+        outDir: 'dist',
     },
-};
+});
